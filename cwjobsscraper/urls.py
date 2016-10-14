31ea45eba5c54cf1f9eb6142c0686df5488207d0
dennis <ezechidc@yahoo.com>
2016-10-14 06:53:25 +0100

    added static url to project.

--- cwjobsscraper/urls.py
+++ cwjobsscraper/urls.py
@@ -7,8 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('cwjobs.urls', namespace='cwjobs')),
-url(r'^contact/', include('contact.urls', namespace='contact')),
+    url(r'^contact/', include('contact.urls', namespace='contact')),
     url(r'^users/', include('users.urls', namespace='users')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
